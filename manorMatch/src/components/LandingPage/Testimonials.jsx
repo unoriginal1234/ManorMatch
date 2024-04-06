0d864b88adb0696b6ed11c84795d42a614ed9c2a
unoriginal1234 <eric.t.roth@gmail.com>
2024-04-06 15:04:31 -0700

    Videos playing and looping on page spinup

--- manorMatch/src/components/LandingPage/Testimonials.jsx
+++ manorMatch/src/components/LandingPage/Testimonials.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
 const Testimonials = () => {
+
+  const [currentTestimonial, setCurrentTestimonial] = useState(0)
+
+  const landingTestimonials = [
+    {quote: 'I do declare this is the best thing ever', source: 'Sir Bool'},
+    {quote: "I'm a cutie pie", source: "Anonymous"}
+  ]
+
+  const nextTestimonial = () => {
+    setCurrentTestimonial((prevTestimonial) => (prevTestimonial + 1) % landingTestimonials.length)
+  }
+
   return (
-    <div className="bg-blue-500 text-white p-4 w-screen text-right">
-      <p className="text-2xl font-bold">I do declare this is the best thing ever</p>
-      <p>-Sir Bool</p>
+    <div className="bg-mmblue text-mmsand p-4 w-screen text-right">
+      <p className="text-2xl font-bold">{landingTestimonials[currentTestimonial].quote}</p>
+      <p>-{landingTestimonials[currentTestimonial].source}</p>
+      <button onClick={nextTestimonial}>Next</button>
     </div>
   );
 };
