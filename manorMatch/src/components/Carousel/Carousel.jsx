46427dcd74e93c9a275bc47a0eca0605f02e6843
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-06 15:19:02 -0700

    removed comment which got left in

--- manorMatch/src/components/Carousel/Carousel.jsx
+++ manorMatch/src/components/Carousel/Carousel.jsx
@@ -3,7 +3,6 @@ import CarouselTile from './CarouselTile.jsx';
 const Carousel = () => {
   return (
     <div className="flex flex-row">
-      // Create a carousel of carouselTile components
       <CarouselTile imageUrl="https://via.placeholder.com/150" />
       <CarouselTile imageUrl="https://via.placeholder.com/150" />
       <CarouselTile imageUrl="https://via.placeholder.com/150" />
