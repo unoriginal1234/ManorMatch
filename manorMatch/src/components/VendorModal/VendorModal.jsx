b9ee3feb82d32752386730f49a77516156742f13
maysieo <maysieocera@gmail.com>
2024-04-06 14:21:48 -0400

    Built out a simple framework for the VendorModal

--- manorMatch/src/components/VendorModal/VendorModal.jsx
+++ manorMatch/src/components/VendorModal/VendorModal.jsx
@@ -4,7 +4,20 @@ const VendorModal = () => {
   return (
     // Add your JSX code here
     <div>
-      This is the vendor modal
+      <h1>Vendor Profile</h1>
+      <img src="https://via.placeholder.com/150" alt="Vendor Logo" />
+      <h2>Vendor Name: Vendor Name</h2>
+      <h2>Summary</h2>
+      <p>Vendor Summary</p>
+      <h2>Specialties:</h2>
+      <li>
+        <ul>Specialty 1</ul>
+        <ul>Specialty 2</ul>
+        <ul>Specialty 3</ul>
+        <ul>Specialty 4</ul>
+      </li>
+
+
     </div>
   );
 };
