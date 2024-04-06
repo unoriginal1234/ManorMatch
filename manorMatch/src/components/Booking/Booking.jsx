263a3f56be343a7af947b2c71f710c82738dfeac
Styles <stylesk11@gmail.com>
2024-04-06 14:56:07 -0700

    rename file + build layout of modal slides

---
+++ manorMatch/src/components/Booking/Booking.jsx
@@ -0,0 +1,68 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+import AddressType from './AddressType';
+import EnterAddress from './EnterAddress';
+import SelectAddress from './SelectAddress';
+
+const Booking = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalPage, setModalPage] = useState(1);
+
+  const [addressType, setAddressType] = useState('');
+  const [address, setAddress] = useState({});
+
+  const goToNextPage = () => {
+    setModalPage(modalPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setModalPage(modalPage - 1);
+  }
+
+  return (
+    <div>
+      <button onClick={() => setModalIsOpen(true)}>BOOK NOW</button>
+      This is Calendar Booking
+      <Modal isOpen={modalIsOpen}>
+        {modalPage === 1 && (
+          <AddressType setAddressType={setAddressType} goToNextPage={goToNextPage}/>
+        )}
+        {modalPage === 2 && (
+          <div>
+            <button onClick={() => goToPreviousPage()}>PREVIOUS</button>
+            {addressType === 'enter' && (
+              <EnterAddress />
+            )}
+            {addressType === 'select' && (
+              <SelectAddress />
+            )}
+            <button onClick={() => goToNextPage()}>NEXT</button>
+          </div>
+        )}
+        {modalPage === 3 && (
+          <div>
+            <button onClick={() => goToPreviousPage()}>PREVIOUS</button>
+            <div>CALENDAR</div>
+            <button onClick={() => goToNextPage()}>NEXT</button>
+          </div>
+        )}
+        {modalPage === 4 && (
+          <div>
+            <button onClick={() => goToPreviousPage()}>PREVIOUS</button>
+            <div>VENDORS</div>
+            <button onClick={() => goToNextPage()}>NEXT</button>
+          </div>
+        )}
+        {modalPage === 5 && (
+          <div>
+            <button onClick={() => goToPreviousPage()}>PREVIOUS</button>
+            <div>CONFIRMATION</div>
+          </div>
+        )}
+        <button onClick={() => setModalIsOpen(false)}>Close Modal</button>
+      </Modal>
+    </div>
+  );
+};
+
+export default Booking;
