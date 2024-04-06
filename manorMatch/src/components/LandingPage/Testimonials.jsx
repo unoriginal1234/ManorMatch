d9679107975be7e4fb0d9293bc7253bdd70490f1
unoriginal1234 <eric.t.roth@gmail.com>
2024-04-06 15:37:14 -0700

    Testimonials scroll

--- manorMatch/src/components/LandingPage/Testimonials.jsx
+++ manorMatch/src/components/LandingPage/Testimonials.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Testimonials = () => {
 
   const [currentTestimonial, setCurrentTestimonial] = useState(0)
 
+  useEffect(()=> startTestimonials, [])
+
   const landingTestimonials = [
     {quote: 'I do declare this is the best thing ever', source: 'Sir Bool'},
-    {quote: "I'm a cutie pie", source: "Anonymous"}
+    {quote: "I'm a cutie pie", source: "Anonymous"},
+    {quote: 'My pool is sparkling!', source: "Andre SoCal"},
+    {quote: 'I do declare!', source: "Zacharia Dorito"}
   ]
 
   const nextTestimonial = () => {
     setCurrentTestimonial((prevTestimonial) => (prevTestimonial + 1) % landingTestimonials.length)
   }
 
+  const startTestimonials = () => {
+    return setInterval(()=> nextTestimonial(), 4000);
+  }
+
   return (
     <div className="bg-mmblue text-mmsand p-4 w-screen text-right">
       <p className="text-2xl font-bold">{landingTestimonials[currentTestimonial].quote}</p>
       <p>-{landingTestimonials[currentTestimonial].source}</p>
-      <button onClick={nextTestimonial}>Next</button>
     </div>
   );
 };
