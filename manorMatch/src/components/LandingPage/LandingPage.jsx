37351e7a32edb410c59e780bb29ad5bfd944f901
unoriginal1234 <eric.t.roth@gmail.com>
2024-04-06 20:44:19 -0700

    Style changes

--- manorMatch/src/components/LandingPage/LandingPage.jsx
+++ manorMatch/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Slideshow from './Slideshow.jsx';
 import Testimonials from './Testimonials.jsx';
 import NavBar from './NavBar.jsx'
+import AnimationTest from './AnimationTest.jsx'
 
 
 const LandingPage = () => {
@@ -10,6 +11,7 @@ const LandingPage = () => {
       <NavBar />
       <Slideshow />
       <Testimonials />
+      <AnimationTest />
     </div>
   );
   };
