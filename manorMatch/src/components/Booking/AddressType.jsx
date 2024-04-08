b89f4cde43fc0d155cf4ce06fb29878fe7675eef
Styles <stylesk11@gmail.com>
2024-04-08 12:00:06 -0700

    give buttons some style

--- manorMatch/src/components/Booking/AddressType.jsx
+++ manorMatch/src/components/Booking/AddressType.jsx
@@ -8,10 +8,18 @@ const AddressType = ({ setAddressType, goToNextPage }) => {
   }
 
   return (
-      <div className="relative w-1/2 h-1/2 bg-mmblue p-6 rounded shadow-lg h-quto mx-auto flex">
-          <button onClick={() => handleClick('enter')}>ENTER ADDRESS</button>
+      <div className="relative w-1/2 h-1/2 bg-mmblue p-6 rounded shadow-lg h-quto mx-auto flex flex-col">
+          <button
+            className="bg-mmcream text-black p-2 rounded h-16 w-64"
+            onClick={() => handleClick('enter')}>
+            ENTER ADDRESS
+          </button>
           <div> OR </div>
-          <button onClick={() => handleClick('select')}>SELECT FROM SAVED ADDRESSES</button>
+          <button
+            className="bg-mmcream text-black p-2 rounded h-16 w-64"
+            onClick={() => handleClick('select')}>
+            SELECT FROM SAVED ADDRESSES
+          </button>
       </div>
   )
 }
