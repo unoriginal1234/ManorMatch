cb6a71c2bf2ff4e11bf0243082516288ac459a22
greg <greg.jenkins1034@gmail.com>
2024-04-08 16:28:45 -0700

    fixed small conflict in index.js

--- manorMatch/server/index.js
+++ manorMatch/server/index.js
@@ -25,9 +25,6 @@ const io = new Server(server, {
   },
 });
 app.use(express.json());
-// app.use(cors({
-//   origin: 'http://localhost:3000'
-// }))
 app.use(morgan('dev'));
 
 app.use(cookieParser());
