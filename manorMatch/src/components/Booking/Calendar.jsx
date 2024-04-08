29307e8c3137eb88d0660de96c2414331a5c7d32
Styles <stylesk11@gmail.com>
2024-04-08 12:00:49 -0700

    add date input

--- manorMatch/src/components/Booking/Calendar.jsx
+++ manorMatch/src/components/Booking/Calendar.jsx
@@ -2,7 +2,7 @@
 const Calendar = () => {
 
   return (
-    <div className="relative w-1/2 h-1/2 bg-mmblue p-6 rounded shadow-lg h-quto mx-auto flex">
+    <div className="bg-mmblue p-6 rounded shadow-lg h-quto mx-auto flex">
       <div>
         CALENDAR
       </div>
