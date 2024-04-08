e0916f3fcf3130359802f39678f336ce389eb8e2
Styles <stylesk11@gmail.com>
2024-04-08 10:11:12 -0700

    add select dropdown

--- manorMatch/src/components/Booking/SelectAddress.jsx
+++ manorMatch/src/components/Booking/SelectAddress.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 
 const SelectAddress = ({ goToNextPage }) => {
 
+  const [selectedAddress, setSelectedAddress] = useState(null);
+
+  const handleClick = () => {
+    if (selectedAddress) {
+      goToNextPage();
+    }
+  }
+
   return (
     <div>
-      SELECT FROM THESE SAVED ADDRESSES:
+      SELECT FROM YOUR SAVED ADDRESSES:
+      <select onChange={(e) => setSelectedAddress(e.target.value)}>
+        <option value="address1">123 Main St</option>
+        <option value="address2">456 Elm St</option>
+        <option value="address3">789 Oak St</option>
+      </select>
+      <button onClick={handleClick}>NEXT</button>
     </div>
   )
 }
