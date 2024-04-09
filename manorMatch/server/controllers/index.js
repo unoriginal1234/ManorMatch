294dc20ac41cdb43d2c79752f055905a46b41a18
Gregory Jenkins <65691944+G-Jenkins@users.noreply.github.com>
2024-04-09 15:51:26 -0500

    Revert "Chat"

--- manorMatch/server/controllers/index.js
+++
@@ -1,3 +0,0 @@
-export * as permissions from './permissions.js'
-export * as vendors from './vendors.js'
-export * as services from './services.js'
--- manorMatch/src/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client'; // importing io function from client library, used to establish connection w/ socket.io server
-
-const URL = 'http://localhost:3000';   // define the URL of the server, in this case, the server is running on localhost:5173
-
-//const url = ''
-
-export const socket = io(URL);   // use io() function to connoect to server at this URL
