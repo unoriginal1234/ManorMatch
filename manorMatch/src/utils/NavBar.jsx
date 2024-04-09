16db4e2065077682676f382da309d8198d0fd121
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-09 13:46:30 -0700

    Remove unneeded word

--- manorMatch/src/utils/NavBar.jsx
+++ manorMatch/src/utils/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({ children }) => {
   return (
-    <div className="bg-mmblue text-mmsand w-screen font">
+    <div className="bg-mmblue text-mmsand w-screen">
       <div className="flex justify-between items-center py-3">
         <Link to="/home" className="text-4xl ml-4">
           <span className="tracking-wider font-serif ">M|M</span>
