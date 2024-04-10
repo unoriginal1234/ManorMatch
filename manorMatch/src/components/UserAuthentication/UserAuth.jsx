4f1f9e834452fdaffb25712193d57dddc80e8575
Amarin Sam <amarinsam@gmail.com>
2024-04-09 20:27:18 -0700

    Updated dark/light mode CSS

--- manorMatch/src/components/UserAuthentication/UserAuth.jsx
+++ manorMatch/src/components/UserAuthentication/UserAuth.jsx
@@ -35,11 +35,11 @@ const UserAuth = () => {
           <form onSubmit={handleLoginSubmit}>
             <div>
               <label className="block py-2 mb-2 text-2xl font-large text-white">Email Address:</label>
-              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700  placeholder-gray-400 dark:text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@example.com" required></input>
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 sm:text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700  placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@example.com" required></input>
             </div>
             <div>
               <label className="block pt-3 mb-2 text-2xl font-large text-white">Password:</label>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700  placeholder-gray-400 dark:text-white focus:ring-blue-500 focus:border-blue-500" placeholder="********" required></input>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 sm:text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700  placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="********" required></input>
             </div>
             <div className="py-6">
               <button type="submit" className="w-full text-gray-900 text-2xl bg-mmsand hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 text-center">Login</button>
