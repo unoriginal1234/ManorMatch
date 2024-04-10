a8d8bac72c9d3983e2964be4ebc80b2d9484d743
greg <greg.jenkins1034@gmail.com>
2024-04-10 16:25:27 -0700

    made Chat button more prominent

--- manorMatch/src/components/LandingPage/ChatStandIn.jsx
+++ manorMatch/src/components/LandingPage/ChatStandIn.jsx
@@ -5,7 +5,7 @@ const ChatStandIn = () => {
     <div className="fixed bottom-0 right-0">
       <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-3 px-6 rounded-full inline-flex items-center opacity-70">
         <MdOutlineRealEstateAgent size={24} />
-        <span className="text-lg">Talk to concierge</span>
+        <span className="text-lg">Talk to dfgf</span>
       </button>
     </div>
   );
