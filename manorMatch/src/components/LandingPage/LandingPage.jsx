de90e741bdd7e132f5de428304bba437f15e127e
Styles <stylesk11@gmail.com>
2024-04-11 11:40:08 -0700

    import Mission and Services components

--- manorMatch/src/components/LandingPage/LandingPage.jsx
+++ manorMatch/src/components/LandingPage/LandingPage.jsx
@@ -1,23 +1,24 @@
 import Slideshow from './Slideshow.jsx';
 import Testimonials from './Testimonials.jsx';
 import Mission from './Mission.jsx';
+import Services from './Services.jsx';
 import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
 
   return (
-    <div className="w-full">
-      <Link to='/home' className="font-serif absolute z-10 text-mmcream left-20 top-12 font-normal text-5xl">
+    <div className="w-full flex flex-col items-center">
+      <Link to='/home' className="font-serif absolute z-10 text-mmcream left-20 top-12 font-normal text-5xl text-outline">
           M | M
       </Link>
       <Link to="/login">
         <button className="bg-mmblue text-2xl text-white font-bold px-8 py-3 absolute z-10 top-12 right-20 rounded-xl hover:scale-110 duration-200 tracking-wider shadow-landinglogin">Log in</button>
       </Link>
-      <div className="flex flex-col items-center justify-center bg-mmblue">
-        <Slideshow />
-        <Testimonials />
-        <Mission />
-      </div>
+      <Slideshow />
+      <Testimonials />
+      <Mission />
+      <div className="bg-mmcream h-40 w-full relative bottom-32"></div>
+      <Services />
     </div>
   )
 };
