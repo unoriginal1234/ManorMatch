a5b7811fb8dd4a456297c6b1a46b626d8db0516c
greg <greg.jenkins1034@gmail.com>
2024-04-11 09:16:05 -0700

    changing styling for confirmation modal

--- manorMatch/src/components/Booking/Confirmation.jsx
+++ manorMatch/src/components/Booking/Confirmation.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 const Confirmation = ({ handleClose, selectedVendor, date, time}) => {
 
   return (
-    <div className="bg-mmblue w-1/2 h-3/5">
-      <div>Your reservation has been added to the cart!</div>
-      <div>Details:</div>
-      <div>{selectedVendor.category}</div>
-      <div>{selectedVendor.name}</div>
-      <div>{date}</div>
-      <div>{time}</div>
-      <button
-        className="bg-mmcream text-mmblue"
-        onClick={() => handleClose()}
-      >Add More Services</button>
-      <Link to='/cart'>
-        <button className="bg-mmcream text-mmblue">View Cart</button>
-      </Link>
-    </div>
+    <div className="bg-mmblue w-1/2 h-3/5 text-center text-xl border-2 border-white-500">
+  <div className="text-4xl py-4">Your reservation has been added to the cart!</div>
+  <div className="text-2xl py-3">Details:</div>
+  <div className="text-xl py-4">{selectedVendor.category}</div>
+  <div className="text-2xl py-2">{selectedVendor.name}</div>
+  <div className="font-bold text-2xl py-1">{date}</div>
+  <div className="font-bold text-2xl py-1">{time}</div>
+  <button
+    className="bg-mmcream text-mmblue px-3"
+    onClick={() => handleClose()}
+  >Add More Services</button>
+  <Link to='/cart'>
+    <button className="bg-mmcream text-mmblue px-3">View Cart</button>
+  </Link>
+</div>
   )
 }
 
