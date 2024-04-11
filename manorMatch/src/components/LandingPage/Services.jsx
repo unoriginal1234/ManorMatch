b8ccf6b2ec88dd0f824a0a59ae25233e995bf9c9
Styles <stylesk11@gmail.com>
2024-04-11 11:42:51 -0700

    stub services component

---
+++ manorMatch/src/components/LandingPage/Services.jsx
@@ -0,0 +1,22 @@
+
+const Services = () => {
+
+  return (
+    <div className="bg-white w-full text-black font-bold flex items-center justify-center relative bottom-32 pt-32">
+    <div>SERVICES</div>
+    <ul>
+      <li>Event Planning</li>
+      <li>Home Repairs</li>
+      <li>Housekeeping</li>
+      <li>Landscaping</li>
+      <li>Personal Chef</li>
+      <li>Pool Maintenance</li>
+      <li>Security</li>
+      <li>Transportation</li>
+      <li>Wine Cellar Management</li>
+    </ul>
+  </div>
+  )
+}
+
+export default Services;
