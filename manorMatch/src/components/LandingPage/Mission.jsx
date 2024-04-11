2e42e0dd9af4979f06809f664a3ed7e5e66b90d3
Styles <stylesk11@gmail.com>
2024-04-10 19:33:37 -0700

    add mission statement to landing page

---
+++ manorMatch/src/components/LandingPage/Mission.jsx
@@ -0,0 +1,26 @@
+const Mission = () => {
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-mmblue">
+      <div className="flex flex-col items-center justify-center w-full h-96 bg-white mt-10">
+        <h1 className="text-3xl font-bold text-mmblue">Manor | Match</h1>
+        <p className="text-lg font-serif text-mmblue w-2/3">At Manor | Match, we pride ourselves on delivering unparalleled property management services tailored to the needs of homeowners with luxury estates and historic properties. Our commitment to excellence ensures that every aspect of upscale home management and maintenance is seamlessly executed through our sophisticated user portal, offering a comprehensive array of services at your fingertips. With a focus on quality, discretion, and personalized attention, we strive to alleviate any concerns you may have, providing peace of mind and allowing you to fully enjoy the splendor of your estate. </p>
+        <div>Services</div>
+        <ul>
+          <li>Housekeeping</li>
+          <li>Landscaping</li>
+          <li>Pool Maintenance</li>
+          <li>Home Repairs</li>
+          <li>Personal Chef</li>
+          <li>Concierge</li>
+          <li>Security</li>
+          <li>Event Planning</li>
+          <li>Transportation</li>
+          <li>Wine Cellar Management</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Mission;
